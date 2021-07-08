695a5e22978d7f217e9594caac55997dbcd2063b
Dorian Donoso <adonosoaster@gmail.com>
2021-07-07 22:25:42 -0400

    first commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const {Product}=require('../models/product.model');
 
 module.exports.createProduct=(request,response)=>{
@@ -17,9 +18,22 @@ module.exports.getAll=(request,response) => {
     .catch(err=>response.json(err))
 }
 
-module.exports.getProduct = (request, response)=>{
+module.exports.getProduct = (request, response) => {
     Product.findOne({_id:request.params.id})
         .then(product => response.json(product))
         .catch(err => response.json(err))
 }
+
+module.exports.updateProduct = (request, response) => {
+    Product.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
+        .then(updateProduct => response.json(updateProduct))
+        .catch(err => response.json(err))
+}
+
+module.exports.deleteProduct = (request, response) => {
+    Product.deleteOne({_id: request.params.id})
+        .then(deleteConfirmation => response.json(deleteConfirmation))
+        .catch(err => response.json(err))
+}
+
 
